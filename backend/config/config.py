6a92e5615c4679450507f757bbf5e0105d0c039b
Sweni002 <nisesweni@gmail.com>
2025-08-16 10:03:20 +0300

    Préparation pour Render

--- backend/config/config.py
+++ backend/config/config.py
@@ -2,14 +2,14 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, '..', '.env'))  # charge le .env
 
 class Config:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
-    SQLALCHEMY_DATABASE_URI = None
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')  # utilise l'URL de Render
+
 
 class DevelopmentConfig(Config):
     DEBUG = True
